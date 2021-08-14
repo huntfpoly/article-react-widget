67c3f1fbba41cc4b33acbc67e465d744a3c2c6e8
hieupv <hieunucek6@gmail.om>
2021-08-14 22:11:09 +0700

    Updates

---
+++ precache-manifest.45c3055fa8bd35d0aa83d411d1a430af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe4454e28900fa98c080b4d0f8c7c92",
+    "url": "/article-react-widget/index.html"
+  },
+  {
+    "revision": "04df208f823c45199ab6",
+    "url": "/article-react-widget/static/css/main.7b2cd885.chunk.css"
+  },
+  {
+    "revision": "55ea78a386c4d76a41cf",
+    "url": "/article-react-widget/static/js/2.de616a92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/article-react-widget/static/js/2.de616a92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04df208f823c45199ab6",
+    "url": "/article-react-widget/static/js/main.66cafafe.chunk.js"
+  },
+  {
+    "revision": "7d257a89a374592ea41b",
+    "url": "/article-react-widget/static/js/runtime-main.1f1f71df.js"
+  }
+]);
